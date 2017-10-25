fa94b23d41872be879c177b95aefac555a1f99aa
Cherié Warren <CheriePhi@msn.com>
2017-10-24 21:21:23 -0700

    Basic data binding

--- AddressBookClient/AddressItem.cs
+++ AddressBookClient/AddressItem.cs
@@ -1,31 +1,43 @@
-﻿namespace ConsoleAddress
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleAddress
 {
+    /// <summary>
+    /// Wraps the domain address book classes into data that can easily bind to a view.
+    /// </summary>
     public class AddressItem
     {
-        private string name;
-        private string street;
-        private string city;
-        private string state;
-        private string zip;
-        private string country;
+        private static AddressBook addressBook;
+        private KeyValuePair<string, Address> addressItem;
 
+        #region Constructors
         public AddressItem() : this("", "", "", "", "", "") {}
 
         public AddressItem(string name, string street, string city, string state, string zip, string country)
         {
-            this.name = name;
-            this.street = street;
-            this.city = city;
-            this.state = state;
-            this.zip = zip;
-            this.country = country;
+            var address = new Address(street, city, state, zip, country);
+            addressBook.Add(name, address);
+
+            var addresses = addressBook.GetAll();
+            addressItem = addresses.FirstOrDefault(pair => pair.Key == name);
         }
 
-        public string Name { get => name; set => name = value; }
-        public string Street { get => street; set => street = value; }            
-        public string City { get => city; set => city = value; }
-        public string State { get => state; set => state = value; }
-        public string Zip { get => zip; set => zip = value; }
-        public string Country { get => country; set => country = value; }
+        public AddressItem(KeyValuePair<string, Address> addressItem)
+        {
+            this.addressItem = addressItem;
+        }
+        #endregion
+
+
+        public static AddressBook AddressItemBook { get => addressBook; set => addressBook = value; }
+
+        public string Name { get => addressItem.Key; set => throw new NotImplementedException(); }
+        public string Street { get => addressItem.Value.getSpec("street"); set => addressBook.Update(addressItem.Key, "street", value); }            
+        public string City { get => addressItem.Value.getSpec("city"); set => addressBook.Update(addressItem.Key, "city", value); }
+        public string State { get => addressItem.Value.getSpec("state"); set => addressBook.Update(addressItem.Key, "state", value); }
+        public string Zip { get => addressItem.Value.getSpec("zip"); set => addressBook.Update(addressItem.Key, "zip", value); }
+        public string Country { get => addressItem.Value.getSpec("country"); set => addressBook.Update(addressItem.Key, "country", value); }
     }
 }
--- AddressBookClient/MainWindow.xaml.cs
+++ AddressBookClient/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -10,9 +12,14 @@ namespace ConsoleAddress
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        private Controller controller;
+        private List<AddressItem> addresses;
+
+    public MainWindow()
         {
             InitializeComponent();
+            controller = new Controller();
+            AddressItem.AddressItemBook = new AddressBook();
         }
 
         #region Menu items
@@ -22,22 +29,35 @@ namespace ConsoleAddress
             if (dialog.ShowDialog() == true)
             {
                 var fileName = dialog.FileName;
+                using (var input = File.OpenRead(fileName))
+                {
+                    var addressBook = controller.Load(input);
+
+                    AddressItem.AddressItemBook = addressBook;
+
+                    throw new NotImplementedException(); // TODO: Need to set the data binding so it refreshes
+                }
             }
         }
 
         private void Save_Click(object sender, RoutedEventArgs e)
         {
-            var dialog = new OpenFileDialog();
+            var dialog = new SaveFileDialog();
             if (dialog.ShowDialog() == true)
             {
                 var fileName = dialog.FileName;
+
+                using (var output = File.Create(fileName))
+                {
+                    controller.Save(output, AddressItem.AddressItemBook);
+                }
             }
         }
         #endregion
 
         private void DataGrid_Loaded(object sender, RoutedEventArgs e)
         {
-            var addresses = new List<AddressItem>{};
+            addresses = new List<AddressItem>{};
             var grid = sender as DataGrid;
             grid.ItemsSource = addresses;
         }
