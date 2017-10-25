f66017762b40aa71ea11bbeb2cfc0e517ca5daef
Cherié Warren <CheriePhi@msn.com>
2017-10-24 20:15:02 -0700

    Skeleton UI

---
+++ AddressBookClient/AddressItem.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleAddress
+{
+    public class AddressItem
+    {
+        private string name;
+        private string street;
+        private string city;
+        private string state;
+        private string zip;
+        private string country;
+
+        public AddressItem() : this("", "", "", "", "", "") {}
+
+        public AddressItem(string name, string street, string city, string state, string zip, string country)
+        {
+            this.name = name;
+            this.street = street;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+            this.country = country;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public string Street { get => street; set => street = value; }            
+        public string City { get => city; set => city = value; }
+        public string State { get => state; set => state = value; }
+        public string Zip { get => zip; set => zip = value; }
+        public string Country { get => country; set => country = value; }
+    }
+}
---
+++ AddressBookClient/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Win32;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ConsoleAddress
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region Menu items
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == true)
+            {
+                var fileName = dialog.FileName;
+            }
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == true)
+            {
+                var fileName = dialog.FileName;
+            }
+        }
+        #endregion
+
+        private void DataGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            var addresses = new List<AddressItem>{};
+            var grid = sender as DataGrid;
+            grid.ItemsSource = addresses;
+        }
+    }
+}
