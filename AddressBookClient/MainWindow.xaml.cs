705c0a965776316762422cd38bb07153a149e052
Cherié Warren <cheriephi@msn.com>
2017-11-06 19:14:03 -0800

    Changed to bind to SQL
    Removed references to the business objects

--- AddressBookClient/MainWindow.xaml.cs
+++ AddressBookClient/MainWindow.xaml.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.Win32;
-using System.IO;
-using System.Windows;
+﻿using System.Windows;
 
 namespace AddressBookClient
 {
@@ -14,35 +12,14 @@ namespace AddressBookClient
         public MainWindow()
         {
             InitializeComponent();
+
             addressBookViewModel = new AddressBookViewModel();
-
             this.DataContext = addressBookViewModel;
-        }
-
-        #region Menu items
-        private void Open_Click(object sender, RoutedEventArgs e)
-        {
-            var dialog = new OpenFileDialog();
-            if (dialog.ShowDialog() == true)
-            {
-                using (var input = File.OpenRead(dialog.FileName))
-                {
-                    addressBookViewModel.Load(input);
-                }
-            }
         }
 
         private void Save_Click(object sender, RoutedEventArgs e)
         {
-            var dialog = new SaveFileDialog();
-            if (dialog.ShowDialog() == true)
-            {
-                using (var output = File.Create(dialog.FileName))
-                {
-                    addressBookViewModel.Save(output);
-                }
-            }
+            addressBookViewModel.Save();
         }
-        #endregion
     }
 }
